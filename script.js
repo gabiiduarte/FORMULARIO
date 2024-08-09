6e19bf075b354fb84f1f5a689b3358ecad9d0b65
gabiiduarte <125580472+gabiiduarte@users.noreply.github.com>
2024-08-09 12:07:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Função de validação do nome
     function validateName() {
-        if (nameInput.value.trim() === '') {
+        const nameValue = nameInput.value.trim();
+        if (nameValue === '') {
             nameError.textContent = 'O nome é obrigatório.';
+        } else if (/\d/.test(nameValue)) {
+            nameError.textContent = 'O nome não pode conter números.';
         } else {
             nameError.textContent = '';
         }
